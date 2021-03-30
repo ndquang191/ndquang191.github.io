e6eb83b2efa950079c2b4c6a9964b4bba0fee9cd
Quang <ndquang191@gmail.com>
2021-03-30 23:55:23 +0700

    lab9

--- lab9/9.js
+++ lab9/9.js
@@ -1,57 +1,57 @@
 //3. Viết chương trình nhập vào bán kính hình tròn, tính và in ra chu vi, diện tích của hình tròn
 
-// let r = Number(prompt("Nhap ban kinh cua hinh tron"))
-// const PI = 3.14;
+let r = Number(prompt("Nhap ban kinh cua hinh tron"))
+const PI = 3.14;
 
-// function chuVi( r){
-//     return 2*PI*r;
-// }
+function chuVi( r){
+    return 2*PI*r;
+}
 
-// function dienTich( r ){
-//     return PI*r*r;
-// } 
+function dienTich( r ){
+    return PI*r*r;
+} 
 
-// console.log( "Chu vi hinh tron la: " + chuVi(r))
-// console.log( "Dien tich hinh tron la: " + dienTich(r))
+console.log( "Chu vi hinh tron la: " + chuVi(r))
+console.log( "Dien tich hinh tron la: " + dienTich(r))
 
 
 // Bai1 : Viết chương trình nhập vào một giá trị đo độ dài ở đơn vị cm, tính và in ra giá trị tương ứng ở
 // các đơn vị mm, m, km
 
-// let r = Number(prompt("Nhap do dai theo don vi cm"))
+let r = Number(prompt("Nhap do dai theo don vi cm"))
 
-// function mm( r ){
-//     return r*10;
-// }
+function mm( r ){
+    return r*10;
+}
 
-// function m( r ){
-//     return r/100;
-// }
+function m( r ){
+    return r/100;
+}
 
-// function km( r ){
-//     return r/100000;
-// }
+function km( r ){
+    return r/100000;
+}
 
-// console.log("Doi sang mm:" + mm( r ))
-// console.log("Doi sang m:" + m( r ))
-// console.log("Doi sang km:" + km( r ))
+console.log("Doi sang mm:" + mm( r ))
+console.log("Doi sang m:" + m( r ))
+console.log("Doi sang km:" + km( r ))
 
 
 // Bai2 : Viết chương trình nhập vào một giá trị nhiệt độ ở đơn vị Celsius, in ra nhiệt độ ở đơn vị Fahrenheit và Kevin tương ứng
 
-// let c = Number(prompt("Nhap nhiet do can doi"))
-// let b = Number(prompt("Nhap nhiet do can doi"))
-// function cToF(c){
-//     return (c*9/5) + 32;
-// }
+let c = Number(prompt("Nhap nhiet do can doi"))
+let b = Number(prompt("Nhap nhiet do can doi"))
+function cToF(c){
+    return (c*9/5) + 32;
+}
 
-// function cToK(c){
-//     return c+ 273.15;
-// }
+function cToK(c){
+    return c+ 273.15;
+}
 
-// console.log( c + " *C doi sang *F : " + cToF(c));
+console.log( c + " *C doi sang *F : " + cToF(c));
 
-// console.log( c + " *C doi sang *K : " + cToK(b));
+console.log( c + " *C doi sang *K : " + cToK(b));
 
 
 //
