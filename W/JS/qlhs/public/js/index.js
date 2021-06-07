2d9cdb75eca55fd94651cf47e7144d0660d7250c
Quang <ndquang191@gmail.com>
2021-06-07 10:57:59 +0700

    jq

--- W/JS/qlhs/public/js/index.js
+++ W/JS/qlhs/public/js/index.js
@@ -1,8 +1,35 @@
-const STUDENT_PER_PAGE = 5;
+import validate from "./validate.js";
+import observer from "./observer.js";
 
-const xhr = new XMLHttpRequest();
+observer.notify("render homepage");
+window.onpopstate = () => observer.notify("render homepage");
 
-xhr.responseText = 'json';
+const studentForm = document.getElementById("student");
+studentForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
 
-let method = 'GET';
-let ur 
+    let data = validate();
+
+    if (data) {
+        fetch("/students", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then(() => {
+                observer.notify("render homepage");
+                studentForm.reset();
+                document.getElementById("close").click();
+            })
+            .catch((e) => console.log(e));
+    }
+});
+
+const search = document.getElementById("search");
+search.addEventListener("search", (ev) => {
+    let value = search.value.trim();
+    history.pushState(null, null, value ? "?q=" + value : "/");
+    observer.notify("render homepage");
+});
